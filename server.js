4fb1df7b800ae0dcceaffcde4da79b3e46ca50ec
Andrew Beers <andybeers@gmail.com>
2017-01-28 12:26:26 -0800

    Removed root level server.js. moved into server dir.

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const app = require('./lib/app');
-const port = process.env.PORT || 3000;
-
-const server = http.createServer(app);
-
-server.listen(port, () => {
-  console.log('Server started on port: ', server.address().port);
-});
