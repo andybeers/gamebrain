142d28dd469d2fd3c62237ab3a7f1b3253dfc624
Andrew Beers <andybeers@gmail.com>
2017-01-29 15:39:06 -0800

    Added jsonwebtoken service and ensureAuth middleware

---
+++ server/lib/auth/ensure-auth.js
@@ -0,0 +1,17 @@
+const tokenSvc = require('./token');
+
+module.exports = function getEnsureAuth() {
+  return function ensureAuth(req, res, next) {
+    if(req.method === 'OPTIONS') return next();
+    const token = req.headers.Authorization;
+    if(!token) return next({ code: 403, error: 'Unauthorized, no token provided'});
+    tokenSvc.verify(token)
+      .then(payload => {
+        req.user = payload;
+        next();
+      })
+      .catch(() => {
+        next({ code: 403, error: 'Unauthorized, bad token'});
+      });
+  };
+};
