02f47d77f8eaec92b73adca116d98571bd6699a3
Andrew Beers <andybeers@gmail.com>
2017-02-01 01:02:52 -0800

    Removed params from bgg routes. bgg now called bggAPI.

--- server/lib/routes/bggAPI.js
+++ server/lib/routes/bggAPI.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-// const bodyParser = require('body-parser').json();
 const superagent = require('superagent');
 const xmlParser = require('../xml-parser');
 
@@ -8,7 +7,7 @@ router
   .get('/search/:query', (req, res, next) => {
     const query = req.params.query;
     superagent
-      .get(`https://www.boardgamegeek.com/xmlapi/search?search=${query}`)
+      .get(`https://www.boardgamegeek.com/xmlapi2/search?query=${query}&type=boardgame,boardgameexpansion`)
       .accept('xml')
       .parse(xmlParser)
       .then(bggResponse => res.send(bggResponse.body))
@@ -17,7 +16,7 @@ router
   .get('/:id', (req, res, next) => {
     const id = req.params.id;
     superagent
-      .get(`https://www.boardgamegeek.com/xmlapi/boardgame/${id}`)
+      .get(`https://www.boardgamegeek.com/xmlapi2/thing?id=${id}`)
       .accept('xml')
       .parse(xmlParser)
       .then(bggResponse => res.send(bggResponse.body))
