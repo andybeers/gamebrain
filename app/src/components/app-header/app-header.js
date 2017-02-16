25db1a5f294409f2809f8dc5ad25ed9349059b70
Andrew Beers <andybeers@gmail.com>
2017-02-16 14:09:05 -0500

    Header nav responsive for small and large displays

--- app/src/components/app-header/app-header.js
+++ app/src/components/app-header/app-header.js
@@ -13,6 +13,8 @@ controller.$inject = ['userService', '$state'];
 
 function controller(userService, $state) {
   this.styles = styles;
+
+  this.tab = 'collection';
   
   this.logout = () => {
     userService.logout();
