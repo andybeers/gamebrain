e274e0c644e863e46e72ed3f86b026ad108f24c1
Andrew Beers <andybeers@gmail.com>
2017-02-26 17:25:14 -0800

    Added back buttons to all add-views. Styling add gamenight form.

--- app/src/components/home/gamenights/add-gamenight/add-gamenight.js
+++ app/src/components/home/gamenights/add-gamenight/add-gamenight.js
@@ -14,4 +14,19 @@ export default {
 function controller() {
   this.styles = styles;
   this.tab = 'gamenights';
+  this.today = new Date();
+  this.date;
+  this.update = () => {
+    this.dateString = this.date.toString();
+  };
+
+  this.create = () => {
+    const gamenight = {
+      name: this.name,
+      description: this.description,
+      date: this.date
+    };
+
+    
+  };
 }
