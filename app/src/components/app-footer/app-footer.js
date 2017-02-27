63355cf2c4e37ee01ae32f4fa9b3d2010128279a
Andrew Beers <andybeers@gmail.com>
2017-02-27 13:20:20 -0800

    Added footer component

---
+++ app/src/components/app-footer/app-footer.js
@@ -0,0 +1,11 @@
+import template from './app-footer.html';
+import styles from './app-footer.scss';
+
+export default {
+  template,
+  controller
+};
+
+function controller() {
+  this.styles = styles;
+}
